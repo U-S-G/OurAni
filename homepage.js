f0a3d3e007fff1278a237b5c824ed4e3b1113b8a
U-S-G <145062517+U-S-G@users.noreply.github.com>
2024-11-14 01:33:42 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,4 +1,5 @@
-function show(){
-    document.querySelector('.hamburger').classList.toggle('open')
-    document.querySelector('.navigation').classList.toggle('active')
+function show() {
+    document.querySelector('.hamburger').classList.toggle('open');
+    document.querySelector('.navigation').classList.toggle('active');
+    document.querySelector('header ul').classList.toggle('show');
 }
