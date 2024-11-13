9d3787029426a1390d5bbf15358c720ca75d0942
U-S-G <145062517+U-S-G@users.noreply.github.com>
2024-11-14 01:36:19 +0200

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,5 @@
+function show() {
+    document.querySelector('.hamburger').classList.toggle('open');
+    document.querySelector('.navigation').classList.toggle('active');
+    document.querySelector('header ul').classList.toggle('show');
+}
