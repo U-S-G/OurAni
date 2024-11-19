cb28060f9a62a89df0142afeef4411ff90e91975
U-S-G <145062517+U-S-G@users.noreply.github.com>
2024-11-19 20:11:48 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,5 +1,4 @@
-function show() {
-    document.querySelector('.hamburger').classList.toggle('open');
-    document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('header ul').classList.toggle('show');
+function show(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
 }
